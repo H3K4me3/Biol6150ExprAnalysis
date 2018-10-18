1e4f4056260ef559b13b41df33ac80b70f491686
Jialin Ma <marlin-@gmx.cn>
2018-10-18 02:58:45 -0400

    Update script to download microarray data

--- script/download_microarray_dataset.sh
+++ script/download_microarray_dataset.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+set -x
+
 # Create and enter dir
 mkdir -p data/microarray
 cd data/microarray
@@ -11,4 +13,6 @@ curl 'https://www.ncbi.nlm.nih.gov/geo/download/?acc=GSE26487&format=file' \
 
 tar xvf GSE26487_RAW.tar
 rm GSE26487_RAW.tar
+
+find . -type f | egrep -v "(GSM651310|GSM651320|GSM651315|GSM651325)" | xargs rm
 
